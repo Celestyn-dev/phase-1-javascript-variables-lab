fe83a62a15c504a08e4814e45a9a0f4cd6799827
Celestine Mecheo <celestinenyamoita094@gmail.com>
2025-03-07 06:25:15 +0300

    Completed Variables Lab

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Code your solution in this file!
+// Declare a constant variable for companyName
+const companyName = "Scuber";
+
+// Declare a variable for mostProfitableNeighborhood using let
+let mostProfitableNeighborhood = "Chelsea";
+
+// Declare a variable for companyCeo using let
+let companyCeo = "Susan Smith";
